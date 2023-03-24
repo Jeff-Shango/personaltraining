7c41dc2137e5aa9f44bc278b83e8fda0d7c37d4e
Jeff B <105983864+Jeff-Shango@users.noreply.github.com>
2023-03-23 23:22:13 -0400

    creating breakdown section

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./Components/Dashboard/Dashboard";
 import Navlinks from "./Components/Navlinks/Navlinks";
 import "./app.css";
+import Breakdown from "./Components/breakdown/Breakdown";
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <Navlinks/>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Dashboard/>}/>
+          <Route path="/" element={<Breakdown/>}/>
         </Routes>
       </BrowserRouter>
+    </div>
     </div>
   );
 }
