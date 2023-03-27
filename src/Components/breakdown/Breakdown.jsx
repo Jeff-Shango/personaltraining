1d274bf69339fd4f8dfbfd6c34a7da63971416d8
Jeff B <105983864+Jeff-Shango@users.noreply.github.com>
2023-03-26 22:05:52 -0400

    updating
    adding in pictures for the breakdown.jsx file.

--- src/Components/breakdown/Breakdown.jsx
+++ src/Components/breakdown/Breakdown.jsx
@@ -4,6 +4,9 @@ import "./breakdown.css";
 const Breakdown = () => {
   return (
     <div id='breakdownContainer'>
+      <div className="picContainer">
+        <div className="picInnerContainer"></div>
+      </div>
       <h2 id='sectionTitle'>What I Do!</h2>
       <p className="sectionText">Working with you wherever you are at, creating personalized plans for your budget and your experience. You are working within a space that is welcoming and has a growth-mindset.</p>
       <button className='breakdownButton'>View Programs</button>
