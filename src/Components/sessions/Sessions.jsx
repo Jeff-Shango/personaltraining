6c7ed7293705998806722b752f6ee79431db1cda
Jeff B <105983864+Jeff-Shango@users.noreply.github.com>
2023-03-27 23:47:16 -0400

    updating calendar functionality
    adding more features to tghe functioning.. just ending on having the api respond in the console log

--- src/Components/sessions/Sessions.jsx
+++ src/Components/sessions/Sessions.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
+import axios from "axios";
 
 const Sessions = () => {
+  const [events, setEvents] = useState([]);
+  useEffect(() => {
+    const fetchEvents = async () => {
+      try{
+        const res = await axios.get("http://localhost:7500/list")
+        setEvents(res.data);
+        console.log(res.data);
+      }catch(err){
+        console.log(err)
+      }
+    }
+    fetchEvents()
+  }, []);
+
   return (
     <div>
       <FullCalendar
@@ -16,6 +31,12 @@ const Sessions = () => {
           end: "dayGridMonth,timeGridWeek,timeGridDay",
         }}
       />
+
+      <div className="container">
+        <h1>Events</h1>
+        <h3>List of events</h3>
+        <p></p>
+      </div>
     </div>
   )
 }
