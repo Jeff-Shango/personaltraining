41a8e64e6276a7e9a3926f79e52626b846f46e02
Jeff B <105983864+Jeff-Shango@users.noreply.github.com>
2023-03-30 16:44:09 -0400

    parq styling
    finishing on basic styling before functioning

--- src/Components/parq/ParQForm.jsx
+++ src/Components/parq/ParQForm.jsx
@@ -39,30 +39,46 @@ const ParQForm = () => {
             <span aria-required="true" id='parqLabel' name="gender">Gender</span>
           </div>
 
-
+          <div className="inputContainer">
             <input id='parqInput' type="text" />            
             <span aria-required="true" id='parqLabel' name="address">Address</span>
+          </div>
 
+          <div className="inputContainer">
             <input id='parqInput' type="text" />
             <span aria-required="true" id='parqLabel' name="town">Town</span>
+          </div>
 
+          <div className="inputContainer">
             <input id='parqInput' type="text" />
             <span aria-required="true" id='parqLabel' name="county">County</span>
+          </div>
 
+          <div className="inputContainer">
             <input id='parqInput' type="number" />
             <span aria-required="true" id='parqLabel' name="zipCode">Zip Code</span>
+          </div>
 
+          <div className="inputContainer">
             <input id='parqInput' type="email" />
             <span aria-required="true" id='parqLabel' name="email">Email</span>
+          </div>
 
+          <div className="inputContainer">
             <input id='parqInput' type="tel" />
             <span aria-required="true" id='parqLabel' name="contactNumber">Contact Number</span>
+          </div>
 
         <div className="emergencyContact">
+        <div className="inputContainer">
             <input id='parqInput' type="text" />
             <span aria-required="true" id='parqLabel' name="emergencyContact">Emergency Contact</span>
+        </div>
+
+        <div className="inputContainer">
             <input id='parqInput' type="tel" />
             <span aria-required="true" id='parqLabel' name="emergencyContactTel">telephone</span>
+        </div>
         </div>
         </section>    
 
@@ -71,7 +87,7 @@ const ParQForm = () => {
             <h2 id='sectionTitle'>Medical Questions</h2>
             <p>Answer the following questions as honestly as you can and provide as much relevant additional information. Answer the following questions by placing a tick in either the <strong>Yes</strong> or <strong>No</strong> boxes (if you should answer <strong>Yes</strong> to any of the questions please provide further information in the space provided)</p>
             <h3 className="descriptionA"><em>Do you currently or have you ever suffered from any of the following conditions?</em></h3>
-            <aside>if <strong>Yes</strong>, please provide details below <input type="text" placeholder='enter Here'/></aside>
+            <aside className='innerInput'>if <strong>Yes</strong>, please provide details below <input type="text"/></aside>
             <ol type='I' className="medicalChecklist">
                 <li>
                     <div class="question">
