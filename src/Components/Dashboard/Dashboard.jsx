bc9c1e0465445bc3431be4891b1d49ce9be8cc8d
Jeff B <105983864+Jeff-Shango@users.noreply.github.com>
2023-05-01 08:31:41 -0400

    styling

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -1,12 +1,75 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import "./dashboard.css";
 import "../../../src/app.css"
 import Carousel from 'react-bootstrap/Carousel';
 
+function animateOnScroll() {
+  const animateElements = document.querySelectorAll(".animate-on-scroll");
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        // Add "in-view" class to the target element
+        entry.target.classList.add("in-view");
+      } else {
+        // Remove "in-view" class from the target element
+        entry.target.classList.remove("in-view");
+      }
+    });
+  }, {
+    threshold: 0.10
+  });
+
+  animateElements.forEach((element) => {
+    // Add the element to the IntersectionObserver
+    observer.observe(element);
+
+    // Set initial CSS values for animation
+    element.style.transform = "translateX(-100%)";
+    element.style.opacity = 0;
+
+    // Add CSS transition property to animate the element
+    element.style.transition = "transform 0.5s ease-out, opacity 0.5s ease-out";
+
+    // Add a class to indicate that the element has been initialized
+    element.classList.add("animate-on-scroll-init");
+  });
+
+  document.addEventListener("scroll", () => {
+    const initElements = document.querySelectorAll(".animate-on-scroll-init");
+
+    initElements.forEach((element) => {
+      // If the element is in view and not yet animated, animate it
+      if (element.classList.contains("in-view") && !element.classList.contains("animated")) {
+        element.style.transform = "translateX(0%)";
+        element.style.opacity = 1;
+        element.classList.add("animated");
+      }
+      // If the element is out of view, reverse the animation
+      else if (!element.classList.contains("in-view") && element.classList.contains("animated")) {
+        element.style.transform = "translateX(-100%)";
+        element.style.opacity = 0;
+        element.classList.remove("animated");
+      }
+    });
+  });
+}
+
+animateOnScroll();
+
+
 const Dashboard = () => {
+
+  useEffect(() => {
+    animateOnScroll()
+    console.log(animateOnScroll())
+  }, [])
+
   return (
     <>
-    <Carousel className='carouselContainer'>
+
+    {/* carousel section: used for identifying most important news based aspects */}
+    <Carousel className='carouselContainer animate-on-scroll'>
       {/* slide 1 */}
       <Carousel.Item className='carItem carItemA'>
         <Carousel.Caption className='captionText captionTextA'>
@@ -33,7 +96,13 @@ const Dashboard = () => {
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
-    <p id='dashboardText'>Get in fighting shape with my personalized coaching and training for boxing and Muay Thai. I offer customized packages to work on technique, strength, and conditioning. Contact me now to get started!</p>
+
+
+
+{/* lower half of the top half of the landing page */}
+    <p id='dashboardText'>
+      Get in fighting shape with my personalized coaching and training for boxing and Muay Thai. I offer customized packages to work on technique, strength, and conditioning. Contact me now to get started!
+      </p>
     <div className="bottomDashboard">
       <button type='button' onClick={() => {window.location.href='http://localhost:3000/programs';}} className="sessions striking">Program Breakdown</button>
       <button type='button' onClick={() => {window.location.href='http://localhost:3000/termsCondition';}} className="sessions packages">Terms and Condition</button>
--- src/Components/breakdown/Breakdown.jsx
+++ src/Components/breakdown/Breakdown.jsx
@@ -3,47 +3,84 @@ import "./breakdown.css";
 import "../../../src/app.css"
 import dashboardGif from "../assets/dashboardGif.mp4"
 
+// function to animate elements from the left to the right (in-view) and reverse
 function animateOnScroll() {
-  // Select all elements with the class "animate-on-scroll"
   const animateElements = document.querySelectorAll(".animate-on-scroll");
 
-  // Create an IntersectionObserver with threshold of 0.05
   const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-          // If the element is in view, add the "in-view" class
-          if (entry.intersectionRatio > 0) {
-              entry.target.classList.add("in-view");
-          }
-          // If the element is out of view, remove the "in-view" class
-          if (entry.intersectionRatio <= 0) {
-              entry.target.classList.remove("in-view");
-              entry.target.classList.add("animate-on-scroll");
-          }
-      });
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        // Add "in-view" class to the target element
+        entry.target.classList.add("in-view");
+      } else {
+        // Remove "in-view" class from the target element
+        entry.target.classList.remove("in-view");
+      }
+    });
+  }, {
+    threshold: 0.10
   });
 
-  // Observe each element with the class "animate-on-scroll"
   animateElements.forEach((element) => {
-      observer.observe(element);
+    // Add the element to the IntersectionObserver
+    observer.observe(element);
+
+    // Set initial CSS values for animation
+    element.style.transform = "translateX(-100%)";
+    element.style.opacity = 0;
+
+    // Add CSS transition property to animate the element
+    element.style.transition = "transform 0.5s ease-out, opacity 0.5s ease-out";
+
+    // Add a class to indicate that the element has been initialized
+    element.classList.add("animate-on-scroll-init");
   });
 
+  document.addEventListener("scroll", () => {
+    const initElements = document.querySelectorAll(".animate-on-scroll-init");
+
+    initElements.forEach((element) => {
+      // If the element is in view and not yet animated, animate it
+      if (element.classList.contains("in-view") && !element.classList.contains("animated")) {
+        element.style.transform = "translateX(0%)";
+        element.style.opacity = 1;
+        element.classList.add("animated");
+      }
+      // If the element is out of view, reverse the animation
+      else if (!element.classList.contains("in-view") && element.classList.contains("animated")) {
+        element.style.transform = "translateX(-100%)";
+        element.style.opacity = 0;
+        element.classList.remove("animated");
+      }
+    });
+  });
 }
 
-animateOnScroll()
+animateOnScroll();
 
 
 const Breakdown = () => {
 
+  useEffect(() => {
+    animateOnScroll();
+  }, []);
   
   return (
     
     <div id='breakdownContainer'>
-      <div class="picContainersWrapper">
+
+      {/* animated Canva flyer of pictures, change based on site traffic */}
+      <section class="picContainersWrapper animate-on-scroll">
         <video src={dashboardGif} id="breakdownGif" className='animate-on-scroll' autoPlay muted loop/>
-      </div>
+      </section>
+    
+
+    {/* lower section for breaking down that services are for everyone */}
+    <section className="breakdownSection animate-on-scroll">
       <h2 class='sectionTitle'>What I Do!</h2>
       <p class="sectionText">Working with you wherever you are at, creating personalized plans for your budget and your experience. You are working within a space that is welcoming and has a growth-mindset.</p>
       <button class='breakdownButton' onClick={() => {window.location.href='http://localhost:3000/programs';}}>View Programs</button>
+    </section>
     </div>
   )
 }
