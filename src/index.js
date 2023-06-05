f1204aa31f80ad897bf57cbdc8fb1f7cda6caa63
Jeff B <105983864+Jeff-Shango@users.noreply.github.com>
2023-06-04 23:56:09 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client'
+// import ReactDOM from 'react-dom';
+// import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// ReactDOM.render(
+//   <BrowserRouter basename="/"><App /></BrowserRouter>,
+//   document.getElementById('root')
+// );
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <App />
+  <App />
 );
